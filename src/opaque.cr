945f61568b4406d4454aff65d9fb09009b14dc16
Zack Kollar <rallokkcaz@gmail.com>
2019-12-15 03:11:02 -0800

    Added global dependency features

--- src/opaque.cr
+++ src/opaque.cr
@@ -1,9 +1,16 @@
+require "logger"
+
 require "./opaque/injector"
-require "./opaque/dependencies"
 require "./opaque/server"
 
 module Opaque
+  include Injector
   VERSION = "0.1.0"
+
+  dependencies do
+    dependency :logger, Logger.new(STDOUT, Logger::INFO)
+  end
+
   server = Server.new()
   server.listen()
 end
--- src/opaque/injector.cr
+++ src/opaque/injector.cr
@@ -1,4 +1,13 @@
 module Opaque::Injector
+  macro dependencies(module_name = "Opaque::Dependencies", &block)
+    module {{module_name.id}}
+      extend self
+      include Injector
+
+      {{block.body}}
+    end
+  end
+
   macro inject(name)
     def {{name.id}}
       Opaque::Dependencies.{{name.id}}
